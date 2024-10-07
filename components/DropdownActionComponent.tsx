0e5c0c4033e2d717ab5e464a131f4383d3cfa007
josh handelman <handelmanjos@gmail.com>
2024-10-07 19:50:14 +0100

    stuff

---
+++ components/DropdownActionComponent.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Button3 } from "./Buttons";
+
+type DropdownActionComponentProps = {
+    img: string;
+    title: string;
+    onChange: (n: number) => void;
+    options: number[];
+    action: () => void;
+    actionText: string;
+};
+export default function DropdownActionComponent({ img, title, options, onChange, action, actionText }: DropdownActionComponentProps) {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selected, setSelected] = useState<number>(options[0]);
+    const swap = () => {
+        if (Number.isNaN(Number(selected))) {
+            action();
+        } else {
+            setIsOpen(!isOpen);
+        }
+    };
+    const change = (n: number) => {
+        setSelected(n);
+        onChange(n);
+    };
+    return (
+        <div className="relative w-[250px] bg-[#373739] flex flex-col gap-4 justify-center items-center pt-14 pb-2 px-2">
+            <div className="absolute w-20 h-20 rounded-full flex justify-center items-center bg-[#6D654E] left-[50%] top-0 -translate-x-[50%] -translate-y-[50%]">
+                <img src={img} className="w-12" />
+            </div>
+            <p>{title}</p>
+            <div className="w-full relative">
+                <Button3 onClick={swap} text={Number.isNaN(Number(selected)) ? actionText : `Selected Server: ${selected}`} />
+                {isOpen &&
+                    <div className="absolute top-full mt-2 left-0 flex flex-col justify-start h-[100px] items-center w-full gap-2 overflow-y-auto">
+                        {options.filter((option) => option !== selected).map((option, i) => {
+                            return (
+                                <div className="w-full" key={i}>
+                                    <Button3 onClick={() => change(option)} text={String(option)} />
+                                </div>
+                            );
+                        })}
+                        <Button3 onClick={action} text={actionText} />
+                    </div>
+                }
+            </div>
+        </div>
+    );
+}

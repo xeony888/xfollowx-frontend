21e711d72d36062e39878e5e1e0a6c5493e17f0e
josh handelman <handelmanjos@gmail.com>
2024-10-16 17:03:06 +0100

    stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
     const TWITTER_CLIENT_ID = "dmhMQWNITWw3RWd5R2p0OTQwRXg6MTpjaQ"; // give your twitter client id here
     const rootUrl = "https://twitter.com/i/oauth2/authorize";
     const options = {
-      redirect_uri: "http://www.localhost:3001/auth/twitter", // client url cannot be http://localhost:3000/ or http://127.0.0.1:3000/
+      redirect_uri: process.env.NEXT_PUBLIC_TWITTER_REDIRECT_URL!, // client url cannot be http://localhost:3000/ or http://127.0.0.1:3000/
       client_id: TWITTER_CLIENT_ID,
       state: user.discordId,
       response_type: "code",
